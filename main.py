48e08259428a4569da7450da18618d296cc13f6f
Loganliotta <loganliotta03@gmail.com>
2026-04-29 01:45:13 +0900

    B: fps working

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
-    screen.fill("black")
-    pygame.display.flip()
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60) 
+        dt = clock.get_time() / 1000.0
+        print(dt)
+
 
 
 if __name__ == "__main__":
